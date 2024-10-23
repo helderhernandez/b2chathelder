9f2daf3aa80a7597b996b24b0d0e2de63f224a2e
helderhernandez <josue10hd@gmail.com>
2024-10-22 21:18:39 -0600

    Primer commit. Se agregaron las dependencias de data-jpa, mysql, validation, web, starter y loombok. Tambien se configuro conexión a DB en el .properties

---
+++ src/main/java/com/b2chat/b2chathelder/B2chathelderApplication.java
@@ -0,0 +1,13 @@
+package com.b2chat.b2chathelder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2chathelderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2chathelderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/b2chat/b2chathelder/B2chathelderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b2chat.b2chathelder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2chathelderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
