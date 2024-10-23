984a8cf71237b387294f69c27ced3e5a54a12af4
helderhernandez <josue10hd@gmail.com>
2024-10-23 10:10:19 -0600

    Swagger dependency configured. Input DTO classes for the user module were documented. HTTP status codes were documented and added to the endpoints of the user module.

--- src/main/java/com/b2chat/b2chathelder/adapters/controller/UserController.java
+++ src/main/java/com/b2chat/b2chathelder/adapters/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.b2chat.b2chathelder.adapters.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,12 +9,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.b2chat.b2chathelder.domain.dto.UserCreateInput;
 import com.b2chat.b2chathelder.domain.dto.UserUpdateInput;
 import com.b2chat.b2chathelder.domain.models.User;
 import com.b2chat.b2chathelder.domain.services.UserService;
+
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/users")
@@ -22,21 +26,29 @@ public class UserController {
 	private UserService userService;
 
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	@Operation(summary = "Create a new user")
 	public User create(@RequestBody UserCreateInput userCreateInput) {
 		return userService.create(userCreateInput);
 	}
 
 	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	@Operation(summary = "Retrieving user by id")
 	public User read(@PathVariable Long id) {
 		return userService.read(id);
 	}
 
 	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	@Operation(summary = "Updating user information")
 	public User update(@PathVariable Long id, @RequestBody UserUpdateInput userUpdateInput) {
 		return userService.update(id, userUpdateInput);
 	}
 
 	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@Operation(summary = "Deleting a user")
 	public void delete(@PathVariable Long id) {
 		userService.delete(id);
 	}
--- src/main/java/com/b2chat/b2chathelder/domain/dto/UserUpdateInput.java
+++ src/main/java/com/b2chat/b2chathelder/domain/dto/UserUpdateInput.java
@@ -1,5 +1,6 @@
 package com.b2chat.b2chathelder.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -12,14 +13,32 @@ import lombok.Data;
 public class UserUpdateInput {
 	@NotBlank
 	@Size(min = 4, max = 25)
-	@Pattern(regexp = "^[A-Za-z0-9_.-]{4,25}$", message = "Formato invalido")
+	@Pattern(regexp = "^[A-Za-z0-9_.-]{4,25}$", message = "Invalid format")
+	@Schema(example = "elena.diaz1994", 
+		//@formatter:off
+		description = "Valid format:"
+			+ "\n - Contain between 4 and 25 characters."
+			+ "\n - May contain upper and lower case letters.."
+			+ "\n - May contain numeric characters."
+			+ "\n - May contain the special characters: . - _")
+		//@formatter:on
 	private String username;
 
 	@NotBlank
 	@Size(min = 8, max = 16)
-	@Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[_*#$.,%+=@-])[A-Za-z0-9_*#$.,%+=@-]{8,16}$", message = "Formato invalido")
+	@Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[_*#$.,%+=@-])[A-Za-z0-9_*#$.,%+=@-]{8,16}$", message = "Invalid format")
+	@Schema(example = "1234.ABcd",
+		//@formatter:off
+		description = "Valid format::"
+			+ "\n - Contain between 8 and 16 characters."
+			+ "\n - Contain at least one uppercase letter"
+			+ "\n - Contain at least one lowercase letter"
+			+ "\n - Contain at least one numeric character."
+			+ "\n - Contain at least one of the following special characters: * # $ . , % + - _ = @")
+		//@formatter:on
 	private String password;
 
 	@NotNull
+	@Schema(example = "true")
 	private Boolean isActive;
 }
